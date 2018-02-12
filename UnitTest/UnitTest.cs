8969e266642ca1387c4fdaf4d7728314e9ade782
Nicholas Sutanto <gp912@cummins.com>
2018-02-12 11:01:38 -0500

    feat: Adding more unit tests

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -55,18 +55,20 @@ namespace UnitTest
         }
 
         [TestMethod]
-        public void TestMethoHotFailSocks()
+        public void TestMethodHotFailJacket()
         {
             DressValidation dressValidation = new DressValidation();
 
-            string input = "HOT 8, 6, 3";
+            string input = "HOT 8, 6, 5";
             string expectedOutput = "Removing PJs, shorts, fail";
             string output = dressValidation.Validate(input);
             Assert.AreEqual(expectedOutput, output);
         }
 
+
+
         [TestMethod]
-        public void TestMethoColdIncomplete()
+        public void TestMethodColdIncomplete()
         {
             DressValidation dressValidation = new DressValidation();
 
@@ -77,11 +79,88 @@ namespace UnitTest
         }
 
         [TestMethod]
+        public void TestMethodColdShoesBeforeSocks()
+        {
+            DressValidation dressValidation = new DressValidation();
+
+            string input = "COLD 8, 6, 1, 3, 4, 2, 5, 7";
+            string expectedOutput = "Removing PJs, pants, fail";
+            string output = dressValidation.Validate(input);
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [TestMethod]
+        public void TestMethodColdShoesBeforePants()
+        {
+            DressValidation dressValidation = new DressValidation();
+
+            string input = "COLD 8, 3, 1, 7";
+            string expectedOutput = "Removing PJs, socks, fail";
+            string output = dressValidation.Validate(input);
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [TestMethod]
+        public void TestMethodColdHeadwearBeforeShirt()
+        {
+            DressValidation dressValidation = new DressValidation();
+
+            string input = "COLD 8, 2";
+            string expectedOutput = "Removing PJs, fail";
+            string output = dressValidation.Validate(input);
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [TestMethod]
+        public void TestMethodHotHeadwearBeforeShirt()
+        {
+            DressValidation dressValidation = new DressValidation();
+
+            string input = "HOT 8, 2";
+            string expectedOutput = "Removing PJs, fail";
+            string output = dressValidation.Validate(input);
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [TestMethod]
+        public void TestMethodColdJacketBeforeShirt()
+        {
+            DressValidation dressValidation = new DressValidation();
+
+            string input = "COLD 8, 5";
+            string expectedOutput = "Removing PJs, fail";
+            string output = dressValidation.Validate(input);
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [TestMethod]
         public void TestColdInvalidRemovePajamas()
         {
             DressValidation dressValidation = new DressValidation();
 
             string input = "COLD 6";
+            string expectedOutput = "fail";
+            string output = dressValidation.Validate(input);
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [TestMethod]
+        public void TestInvalidWeather()
+        {
+            DressValidation dressValidation = new DressValidation();
+
+            string input = "FALL 8, 6, 4, 2, 1, 7";
+            string expectedOutput = "fail";
+            string output = dressValidation.Validate(input);
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [TestMethod]
+        public void TestInvalidCommand()
+        {
+            DressValidation dressValidation = new DressValidation();
+
+            string input = "FALL 8, 6, 4, 2, 9, 1, 7";
             string expectedOutput = "fail";
             string output = dressValidation.Validate(input);
             Assert.AreEqual(expectedOutput, output);
