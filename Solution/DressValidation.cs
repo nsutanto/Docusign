2c914a6f37fc0bce598ce0d6db381c91086ca3b8
Nicholas Sutanto <gp912@cummins.com>
2018-02-12 10:42:03 -0500

    feat: Implement Dress Validation

---
+++ Solution/DressValidation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Solution
+{
+    public class DressValidation : IDressValidation
+    {
+        public DressValidation()
+        {
+            
+        }
+
+        public string Validate(string commandLineString)
+        {
+            string output = "";
+            try
+            {
+                DressCommand dressCommand = CommandFactory.Instance.CreateDressCommand(commandLineString);
+
+                if (dressCommand != null)
+                {
+                    output = dressCommand.Validate();
+                }
+                else
+                {
+                    // not sure what is the requirement for invalid inputs.. Make it fail for now.
+                    output = "fail";
+                }
+            }
+            catch (Exception ex)
+            {
+                // Got exception... let's just output it for now
+                output = ex.Message;
+            }
+
+            return output;
+        }
+    }
+}
