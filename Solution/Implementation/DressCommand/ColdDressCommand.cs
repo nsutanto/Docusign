b701be8c451d2e74c0073a189f4f74069248a19d
Nicholas Sutanto <gp912@cummins.com>
2018-02-12 10:40:41 -0500

    chore: Move DressCommand to DressCommand folder

---
+++ Solution/Implementation/DressCommand/ColdDressCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+    // Cold command implementations
+    internal class ColdDressCommand : DressCommand
+    {
+        internal ColdDressCommand(List<COMMAND_ENUM> commandList, EnumToStringConverter enumToStringConverter) 
+            : base(commandList, enumToStringConverter)
+        {
+        
+        }
+
+        override protected bool ValidateLeaveHouse()
+        {
+            bool isValid = false;
+            // Assume leave house is the last one, so we have 7 commands
+            if (IssuedCommandList.Count == 7)
+            {
+                IssuedCommandList.Add(COMMAND_ENUM.LEAVE_HOUSE);
+                isValid = true;
+            }
+            else
+            {
+                IssuedCommandList.Add(COMMAND_ENUM.FAIL);
+                isValid = false;
+            }
+            return isValid;
+        }
+    }
+}
---
+++ Solution/Implementation/DressCommand/HotDressCommand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+    // Hot Command implementation
+    internal class HotDressCommand : DressCommand
+    {
+        internal HotDressCommand(List<COMMAND_ENUM> commandList, EnumToStringConverter enumToStringConverter) 
+            : base(commandList, enumToStringConverter)
+        {
+            
+        }
+
+        // No jacket during summer
+        override protected bool ValidateJacket()
+        {
+            IssuedCommandList.Add(COMMAND_ENUM.FAIL);
+            return false;
+        }
+
+        // No socks during summer
+        override protected bool ValidateSocks()
+        {
+            IssuedCommandList.Add(COMMAND_ENUM.FAIL);
+            return false;
+        }
+
+        // Perform Validate Footwear
+        override protected bool ValidateFootwear()
+        {
+            bool isValid = false;
+            // Need to wear socks and pants before footwear
+            if (IssuedCommandList.Contains(COMMAND_ENUM.PUT_ON_PANTS))
+            {
+                IssuedCommandList.Add(COMMAND_ENUM.PUT_ON_FOOTWEAR);
+                isValid = true;
+            }
+            else
+            {
+                IssuedCommandList.Add(COMMAND_ENUM.FAIL);
+                isValid = false;
+            }
+            return isValid;
+        }
+
+        override protected bool ValidateLeaveHouse()
+        {
+            bool isValid = false;
+            // Assume leave house is the last one, so we have 5 commands
+            if (IssuedCommandList.Count == 5)
+            {
+                IssuedCommandList.Add(COMMAND_ENUM.LEAVE_HOUSE);
+                isValid = true;
+            }
+            else
+            {
+                IssuedCommandList.Add(COMMAND_ENUM.FAIL);
+                isValid = false;
+            }
+            return isValid;
+        }
+    }
+}
