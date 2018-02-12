9782bc3b4f49c793bbff51222482c53ab8046763
nsutanto <nicholas.sutanto@gmail.com>
2018-02-11 23:35:15 -0500

    feat: Implement Dress Validation

---
+++ Solution/Implementation/DressValidation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Solution
+{
+    public class DressValidation : IDressValidation
+    {
+        public DressValidation()
+        {
+            
+        }
+
+        public string Validate(string commandLineString)
+        {
+            DressCommand dressCommand = CommandFactory.Instance.CreateDressCommand(commandLineString);
+
+            string output = dressCommand.Validate();
+
+            return output;
+        }
+    }
+}
